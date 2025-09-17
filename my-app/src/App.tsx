752140e8216d45c8195fb68d482cde87dc1491a6
BragXyx58 <BragX_yx58@student.itstep.org>
2025-09-17 22:15:30 +0200

    Initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="my-class">
+      <h1>Hello, I'm Oleksandr Brahin</h1>
+      <p>19 years old</p>
+    </div>
+  )
+}
+
+export default App
